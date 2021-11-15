80c57bea56f2119647d0d23b193bd41de277310c
CyanSalt <c.salt@hotmail.com>
2021-11-15 21:25:40 +0800

    feat: support yarn

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,12 @@
-const { getOutdatedPackages } = require('./cli');
+const { getNPMClient, getOutdatedPackages } = require('./cli');
 
 (async () => {
   const outdated = await getOutdatedPackages()
   if (outdated.length) {
+    const npmClient = getNPMClient({})
     console.warn([
       'Some ESLint packages are outdated. You can run the command below to update them:\n',
-      '  npx @cyansalt/eslint-config --update\n',
+      `  ${npmClient === 'yarn' ? 'yarn ' : 'npx @cyansalt/'}eslint-config --update\n`,
     ].join('\n'))
   }
 })()
