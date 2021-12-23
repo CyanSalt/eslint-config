634317254027bd8129359cc59ebb3aca8fce7693
CyanSalt <c.salt@hotmail.com>
2021-12-23 21:09:51 +0800

    fix: allow plain style block in vue 2

--- vue/scoped-css/v2.js
+++ vue/scoped-css/v2.js
@@ -3,6 +3,8 @@ module.exports = {
     'plugin:vue-scoped-css/recommended',
   ],
   rules: {
+    // [覆盖 recommended] 允许使用非 scoped 和 module 的 style 块
+    'vue-scoped-css/enforce-style-type': 'off',
     // [覆盖 recommended] 允许使用未出现过的选择器
     'vue-scoped-css/no-unused-selector': 'off',
   },
