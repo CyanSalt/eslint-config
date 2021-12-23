ea605eed6470e951b4faf7b700679652374acbe8
CyanSalt <c.salt@hotmail.com>
2021-12-23 20:38:21 +0800

    fix: allow unused vue selector

--- vue/scoped-css/v2.js
+++ vue/scoped-css/v2.js
@@ -2,4 +2,8 @@ module.exports = {
   extends: [
     'plugin:vue-scoped-css/recommended',
   ],
+  rules: {
+    // [覆盖 recommended] 允许使用未出现过的选择器
+    'vue-scoped-css/no-unused-selector': 'off',
+  },
 }
