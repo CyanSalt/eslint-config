ac86a751da4b47df0999ded1fe6244f933ff36e0
CyanSalt <c.salt@hotmail.com>
2022-11-08 18:11:37 +0800

    fix: auto remove .ts

--- galaxy/index.js
+++ galaxy/index.js
@@ -3,6 +3,8 @@ module.exports = {
     'plugin:galaxy/recommended',
   ],
   rules: {
+    // 自动移除 .ts 文件的导入后缀
+    'galaxy/import-extensions': ['warn', { '.ts': 'never' }],
     // 自动为 if-else 结构增加花括号
     'galaxy/multi-branch-curly': 'warn',
     // 禁止 3 层及以上的解构赋值
--- galaxy/vue.js
+++ galaxy/vue.js
@@ -3,6 +3,7 @@ module.exports = {
     'plugin:galaxy/recommended-vue',
   ],
   rules: {
+    // 自动调整组件选项内的属性顺序
     'galaxy/conventional-vue-keys-order': ['warn', {
       rules: [
         'model',
@@ -18,7 +19,9 @@ module.exports = {
         'methods-strict',
       ],
     }],
-    'galaxy/import-extensions': ['warn', { '.vue': 'always' }],
+    // 自动为 .vue 导入添加后缀
+    'galaxy/import-extensions': ['warn', { '.ts': 'never', '.vue': 'always' }],
+    // 自动移除模板引号内侧的空格
     'galaxy/vue-attribute-spacing': 'warn',
   },
 }
