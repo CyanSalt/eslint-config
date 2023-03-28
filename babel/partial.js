4632398533f6c9eda27b5e2e4d1cad240e56e0f7
CyanSalt <c.salt@hotmail.com>
2023-03-28 10:38:40 +0800

    fix: add partial babel rules

---
+++ babel/partial.js
@@ -0,0 +1,13 @@
+const config = require('./index.js')
+
+module.exports = {
+  parser: '@babel/eslint-parser',
+  plugins: [
+    '@babel',
+  ],
+  rules: {
+    // 禁止使用某些表达式代替对应语句
+    'no-unused-expressions': 'off',
+    '@babel/no-unused-expressions': config.rules['no-unused-expressions'],
+  },
+}
