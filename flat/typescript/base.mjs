7970f59a712ea5d5244021190b8f7fd2bd234575
CyanSalt <c.salt@hotmail.com>
2024-04-08 19:38:44 +0800

    fix: re-enable eslint rules for ts

--- flat/typescript/base.mjs
+++ flat/typescript/base.mjs
@@ -1,6 +1,6 @@
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
-import typescriptEslintParser from '@typescript-eslint/parser';
-import { PATTERN_ALL } from '../pattern.mjs';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptEslintParser from '@typescript-eslint/parser'
+import { PATTERN_ALL } from '../pattern.mjs'
 
 export default [
   {
@@ -22,5 +22,25 @@ export default [
     plugins: {
       '@typescript-eslint': typescriptEslintPlugin,
     },
+    rules: {
+      'constructor-super': 'error',
+      'getter-return': 'error',
+      'no-const-assign': 'error',
+      'no-dupe-args': 'error',
+      'no-dupe-class-members': 'error',
+      'no-dupe-keys': 'error',
+      'no-func-assign': 'error',
+      'no-import-assign': 'error',
+      'no-new-symbol': 'error',
+      'no-obj-calls': 'error',
+      // TS overload
+      // 'no-redeclare': 'error',
+      'no-setter-return': 'error',
+      'no-this-before-super': 'error',
+      // Global namespaces
+      // 'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-unsafe-negation': 'error',
+    },
   },
-];
+]
--- typescript/base.js
+++ typescript/base.js
@@ -5,4 +5,24 @@ module.exports = {
       jsx: true,
     },
   },
+  rules: {
+    'constructor-super': 'error',
+    'getter-return': 'error',
+    'no-const-assign': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-class-members': 'error',
+    'no-dupe-keys': 'error',
+    'no-func-assign': 'error',
+    'no-import-assign': 'error',
+    'no-new-symbol': 'error',
+    'no-obj-calls': 'error',
+    // TS overload
+    // 'no-redeclare': 'error',
+    'no-setter-return': 'error',
+    'no-this-before-super': 'error',
+    // Global namespaces
+    // 'no-undef': 'error',
+    'no-unreachable': 'error',
+    'no-unsafe-negation': 'error',
+  },
 }
